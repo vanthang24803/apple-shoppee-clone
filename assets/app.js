8fd8aa5383fc9fbff544add89c44232a541a0df3
vanthang24803 <nguyenthang24803@gmail.com>
2023-01-26 10:16:53 +0700

    clone shop

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+var array = {
+  name2: 'javascript',
+  price : 1000,
+  children: {
+    name : 'reactjs'
+  }
+}
+
+var { name2 , children : { name3 } } = array;
+
+console.log(newoject);
+console.log(name3);
